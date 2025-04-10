e2ef459891dd66c49efb933700b6c8f6a952f3f0
gigg-ity <ahmad_cheema94@hotmail.com>
2025-04-10 18:00:28 -0400

    Finished Lab Step 1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let person = {
+let student = {
     name: "John",
     age: 30,
     isenrolled: true,
     greet: function(){
-        console.log("Hi, my name is " + this.name + "and I am " + age);
+        console.log("Hi, my name is " + this.name + "and I am " + this.age);
     }
 
 };
 
 
-console.log(person.greet);
+console.log(student.greet());
