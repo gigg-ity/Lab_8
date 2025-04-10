75490b0029a8a724ff03aab82b98cace4f7ff200
gigg-ity <ahmad_cheema94@hotmail.com>
2025-04-10 17:17:16 -0400

    Added an object to Script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let person = {
     name: "John",
     age: 30,
+    isenrolled: true,
+    greet: function(){
+        console.log("Hi, my name is " + this.name + "and I am " + age);
+    }
 
-}
+};
+
+
+console.log(person.greet);
