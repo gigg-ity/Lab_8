8aa9b5a2838acf93ad08e6b7255d4332ba7df63d
gigg-ity <ahmad_cheema94@hotmail.com>
2025-04-10 20:12:41 -0400

    Finished Part 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let student = {
-    name: "John",
-    age: 30,
+    name: "Ahmad",
+    age: 29,
+    courses: ['Web development', 'Javascript', 'Operating Systems', 'Database Management'],
     isenrolled: true,
     greet: function(){
         console.log("Hi, my name is " + this.name + "and I am " + this.age);
@@ -8,5 +9,10 @@ let student = {
 
 };
 
-
+console.log(student.age);
+console.log(student.name);
 console.log(student.greet());
+
+let jsonStringObject = '{"name":"Ahmad","age":29}';
+let jsonObj = JSON.parse(jsonStringObject);
+console.log(jsonObj.name);
